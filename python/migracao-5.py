b6c8abd4700019be9c783d08259249b1916040b7
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2025-01-16 13:14:18 -0300

    🚧 migração

---
+++ python/migracao-5.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+# Variável de controle para interrupção
+interrupted = False
+arquivo_01 = "./listas/3-lista-videos/videos.csv"
+arquivo_02 = "./listas/4-lista-metadados/metadados.csv"
+arquivo_03 = "./listas/lista-genero-provedor/grupo.csv"
+arquivo_04 = "./listas/3-lista-videos/videos-2.csv"
+
+# Carregar os arquivos CSV
+df_tam  = pd.read_csv(arquivo_01)
+df_meta = pd.read_csv(arquivo_02)
+df_prov = pd.read_csv(arquivo_03)
+df_base = pd.read_csv(arquivo_04)
+
+# Definir os merges a serem realizados, incluindo as colunas necessárias para cada arquivo
+merges = [
+    (df_tam[['link', 'tamanho_GB']], 'link', 'tamanho_GB'),
+    (df_meta[['link', 'largura', 'altura', 'fps', 'contagem quadros']], 'link', ['largura', 'altura', 'fps', 'contagem quadros']),
+    (df_prov[['link', 'date', 'provedor', 'generos', 'titulo']], 'link', ['date', 'provedor', 'generos', 'titulo'])
+]
+
+# Iterar sobre os merges e aplicar as atualizações
+for merge_df, on_col, cols_to_update in merges:
+    merged_df = pd.merge(df_base, merge_df, on=on_col, how='left', suffixes=('', f'_{on_col}'))
+    
+    # Se as colunas para atualizar forem uma lista, atualize todas
+    if isinstance(cols_to_update, list):
+        for col in cols_to_update:
+            df_base[col] = merged_df[f'{col}_{on_col}']
+    else:
+        # Caso contrário, atualize a coluna única
+        df_base[cols_to_update] = merged_df[f'{cols_to_update}_{on_col}']
+
+# Salvar os dados finais antes de sair
+df_base = df_base.sort_values(by=['validade', 'name'], ascending=True)
+df_base.reset_index(drop=True, inplace=True)
+df_base.to_csv(arquivo_04, index=False, encoding='utf-8')
+
+print("Processo finalizado e dados salvos.")
