b747e3a76956ed6e7dc6425deec2e433cb454837
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2025-01-27 18:20:20 -0300

    🚧 dados

--- python/chatgpt.py
+++ python/chatgpt.py
@@ -13,7 +13,7 @@ def consultar_chatgpt(nomes, api_key):
 
     print("Executando requisição")
     # Definir o prompt com o nome do filme
-    prompt = f"Quais são os provedores onde pode ser assistido, gêneros, data de lançamento e titulo conhecido das midias: {nomes}? Responda no formato n=(midia passada na pesquisa mantendo - antes e depois e entre ()) provedores=(lista provedores max 2 entre ()) gêneros=(lista gêneros entre ()) data_lancamento=(yyyy-mm-dd entre ()) titulo=(titulo conhecido em português entre ()), separando cada filme por *, não imprima nada alem disso"
+    prompt = f"Quais são os provedores onde pode ser assistido, gêneros, data de lançamento e titulo conhecido das midias: {nomes}? Responda no formato n=(midia passada na pesquisa mantendo - antes e depois e entre parenteses) provedores=(lista provedores max 2 entre ()) gêneros=(lista gêneros entre ()) data_lancamento=(yyyy-mm-dd entre ()) titulo=(titulo conhecido, entre () e em português ou como é mais facil encontrado na internet ), separando cada filme por *, não imprima nada alem disso, não esqueça os ()"
     
     # Corpo da requisição
     data = {
@@ -64,11 +64,22 @@ def processar_resposta(resposta, dados):
 
     # Substitua 'sua_api_key_aqui' pela sua chave da API
 keys = [
-    # 13:55h
+    # gtvzin.n7
     "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",
-    # 14h
+    # matuedamata123
     "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",
-    "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"
+    # agnesurssula
+    "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",
+    # alljelly
+    "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",
+    # arthurcoelhoestevao442
+    "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",
+    # arthurcoelho442zip
+    "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",
+    # mocic14718@eluxeer.com
+    "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",
+    # lelabas955@eluxeer.com
+    "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",
 ]
 
 # Criar uma lista para armazenar as informações dos filmes
@@ -84,32 +95,56 @@ df_unique   = df_filtered.drop_duplicates(subset='name', keep='first')
 midias = list(df_unique['name'])
 
 passo = 2
+init  = 0
 dados = []
 try:
     while (True):
+        dados.clear()
         for api_key in keys:
-            for i in range(0, len(midias), passo):
+            for i in range(init, len(midias), passo):
                 nomes = midias[i:i+passo]
+                init = i
                 
                 if nomes is None:
                     continue
                 resposta = consultar_chatgpt(nomes, api_key)
                 if resposta is None:
-                    print(i)
                     break
                 
                 # Processar a resposta para extrair provedores, gêneros e data de lançamento
                 processar_resposta(resposta, dados)
-                sleep(21)
-        sleep(60*30)
+                if len(dados) > 500:
+                    break
+                else:
+                    sleep(20)
+        print(dados)
+        for dado in dados:
+            df.loc[df['name'] == dado[0], ['provedor','generos', 'date', 'titulo']] = [dado[1], dado[2], dado[3], dado[4]]
+            
+        # Preenchendo o provedor baseado no título
+        df["provedor"]  = df.groupby("name")["provedor"].transform("first")
+        df["generos"]   = df.groupby("name")["generos"].transform("first")
+        
+        # Salvar os dados finais antes de sair
+        df = df.sort_values(by=['validade', 'name'], ascending=True)
+        df.reset_index(drop=True, inplace=True)
+        df.to_csv(arquivo_out, index=False, encoding='utf-8')
+
+        print(f"Dados exportados para '{arquivo_out}'")
+        
+        if init >= len(midias):
+            print("Todos os dados foram alocados, finalizando")
+            break
+        print("Dormindo ...")
+        print(f"Quantidade de dados que faltam {len(midias)-init}")     
+        sleep(60*2)
 except KeyboardInterrupt:
     print("interrompido")
-    
+
 print(dados)
 for dado in dados:
     df.loc[df['name'] == dado[0], ['provedor','generos', 'date', 'titulo']] = [dado[1], dado[2], dado[3], dado[4]]
-    
- # Preenchendo o provedor baseado no título
+# Preenchendo o provedor baseado no título
 df["provedor"]  = df.groupby("name")["provedor"].transform("first")
 df["generos"]   = df.groupby("name")["generos"].transform("first")
 
@@ -119,5 +154,5 @@ df.reset_index(drop=True, inplace=True)
 df.to_csv(arquivo_out, index=False, encoding='utf-8')
 
 print(f"Dados exportados para '{arquivo_out}'")
-
+print(f"Quantidade de dados que faltam {len(midias)-init}")     
 # 'não disponível'
